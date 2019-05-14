5cdc5bdcf6a182a1208d1225d8cd52077f91f2c6
ThundaHorse <abeeeeekim@gmail.com>
2019-05-14 11:10:18 -0500

    added authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_152835) do
+ActiveRecord::Schema.define(version: 2019_05_14_152029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,13 @@ ActiveRecord::Schema.define(version: 2019_05_13_152835) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "suppliers"
 end
