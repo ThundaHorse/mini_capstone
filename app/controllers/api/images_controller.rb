3e78ff86afdb76ccf0e23ae957dc31ae09760ce0
ThundaHorse <abeeeeekim@gmail.com>
2019-05-13 22:46:14 -0500

    fixed images/suppliers controllers/routes

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,34 +1,42 @@
 class Api::ImagesController < ApplicationController
-  def index 
+  def index
+    @images = Image.all
+    render 'index.json.jbuilder'
+  end
 
-  end 
-
-  def create 
+  def create
     @image = Image.new(
-                      product_id: params[:product_id], 
+                      product_id: params[:product_id],
                       url: params[:image_url]
                       )
+    if @image.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
-    @image.save 
-    render 'show.json.jbuilder' 
-  end 
+  def show
+    @image = Image.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 
+  def update
+    @image = image.find(params[:id])
+    
+    @image.url = params[:url] || @image.url
+    @image.product_id = params[:product_id] || @image.product_id
 
-  def show 
+    if @image.save
+      render 'show.json.jbuilder'
+    else
+      render json: { message: @image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
-
-  end 
-
-
-  def update 
-
-
-  end 
-
-
-  def destroy 
-
-
-  end 
-
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    render json: {message: "Successfully Destroyed Image"}
+  end
 end
--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -1,30 +1,46 @@
 class Api::SuppliersController < ApplicationController
-  def index 
+  def index
+    @suppliers = Supplier.all
+    render 'index.json.jbuilder'
+  end
 
+  def create
+    @supplier = Supplier.new(
+                              name: params[:name],
+                              email: params[:email],
+                              phone_number: params[:phone_number]
+                             )
 
-  end 
+    if @supplier.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @supplier.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
+  def show
+    @supplier = Supplier.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 
-  def create 
+  def update
+    @supplier = Supplier.find(params[:id])
+    
+    @supplier.name = params[:name] || @supplier.name
+    @supplier.email = params[:email] || @supplier.email
+    @supplier.phone_number = params[:phone_number] || @supplier.phone_number
 
+    if @supplier.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @supplier.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
-  end 
+  def destroy
+    @supplier = Supplier.find(params[:id])
+    @supplier.destroy
+    render json: {message: "Successfully Destroyed Supplier"}
+  end
 
-
-  def show 
-
-
-  end 
-
-
-  def update 
-
-
-  end 
-
-
-  def destroy 
-
-
-  end 
 end
